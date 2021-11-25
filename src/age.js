494809a2d4b5af2edfa495bd91c8ceaa227e63ce
andrewc2020 <andrewc2020@gmail.com>
2021-11-25 19:08:42 +0000

    added datesamples

--- src/age.js
+++ src/age.js
@@ -6,4 +6,8 @@ function _calculateAge(birthday) { // birthday is a date
     return Math.abs(ageDate.getUTCFullYear() - 1970);
 }
 
-console.log('my age is', _calculateAge(new Date('05/12/1955')));
+console.log('my age is', _calculateAge(new Date('12/05/1955')));
+
+console.log('Frances age is', _calculateAge(new Date('10/14/1959')));
+
+console.log('Mums age is', _calculateAge(new Date('10/13/1926')));
